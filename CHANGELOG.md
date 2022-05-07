6d31b1982ff40811b0d06f5539862f2b7480513f
Sepehr Karimi <karimi.sepehr2@gmail.com>
2022-05-07 14:55:18 +0430

    v 0.1.3

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+## 0.1.3 (May 7, 2022)
+
+- `onDidMount` and `onDidUnMount` was added to field schema.
+- examples/react-app added.
+- documents updated.
+
+## 0.1.2 (May 4, 2022)
+
+- Some documents added.
+
+## 0.1.1 (May 3, 2022)
+
+- initialize package and base implementations.
--- src/components/elementParser/index.tsx
+++ src/components/elementParser/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Controller, FieldValues, useFormContext } from "react-hook-form";
 import { IElementParser } from "../../types";
 import { isFunction } from "../../utils/helpers";
@@ -7,12 +7,29 @@ import { FormBuilderContext } from "../formBuilderProvider";
 const ElementParser = <TFieldValues extends FieldValues = FieldValues>({ schema }: IElementParser<TFieldValues>) => {
   const formBuilderContext: any = React.useContext(FormBuilderContext);
   const formProps = useFormContext(); // retrieve all hook methods
-
+  const { props, onDidMount, onDidUnMount } = schema;
   const Element = formBuilderContext?.elements?.find((a: any) => a.key === schema?.elementType)?.element;
+
+  const schemaProps = props && isFunction(props) ? props(formProps) : props;
+
+  /**
+   * onDidMount is a function that will be called after the field is mounted.
+   * onDidUnMount is a function that will be called after the field is unmounted.
+   */
+  useEffect(() => {
+    if (onDidMount && isFunction(onDidMount)) {
+      onDidMount(formProps);
+    }
+
+    return () => {
+      if (onDidUnMount && isFunction(onDidUnMount)) {
+        onDidUnMount(formProps);
+      }
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [onDidMount, onDidUnMount]);
+
   if (!Element) return <></>;
-
-  const schemaProps = schema.props && isFunction(schema.props) ? schema.props(formProps) : schema.props;
-
   if (schema.ignoreController) return <Element {...schemaProps} />;
 
   return (
